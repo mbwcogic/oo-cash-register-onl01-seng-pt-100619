609ff90be964e76a89dd2dbf1169c1bbfac6ad68
Michael Waldo <mbwcogic@yahoo.com>
2019-11-13 00:29:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,7 +20,43 @@ class CashRegister
   def total
     @total
   end
+ def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
+    end
 
+    self.last_transaction_amount = amount * quantity
+  end
+
+  def apply_discount
+    if discount != 0
+      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+    else
+      puts "There is no discount to apply."
+    end
+  end
+
+  def items=(list)
+    @items = list
+  end
+
+  def items
+    @items
+  end
+
+  def last_transaction_amount=(amount)
+    @last_transaction_amount = amount
+  end
+
+  def last_transaction_amount
+    @last_transaction_amount
+  end
+
+  def void_last_transaction
+    self.total -= last_transaction_amount
+  end
+end
 
 end
 
